f9c0c02151739ae89207965e8ac451f5a7b8b763
unknown <oluwadareseyii@gmail.com>
2020-11-03 19:01:12 +0100

    change metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Feminist Coalition.`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `The feminist coalition is a group of young Nigerian feminists formed in July 2020 with a mission to champion equality for women in Nigerian society with a core focus on education, financial freedom and representation in public office.`,
     author: `@bethel`,
   },
   plugins: [
